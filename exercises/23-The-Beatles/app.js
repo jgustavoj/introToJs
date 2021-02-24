0cbad3687245cc3952a3053ce8c2382a312f2626
jgustavoj <gjime011@fiu.edu>
2021-02-24 02:08:05 +0000

    practice

--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -1,4 +1,20 @@
-
+function sing(){
+    let array = [];
+    
+    for(let i=0; i < 12; i++){
+        if(i === 4){
+            array.push("words of wisdom");
+        
+        }else if(i == 10){
+            array.push("there will be an answer");
+        }
+        else {
+            array.push("let it be");
+        }
+    }
+    
+    return array.join(', ');
+}
 
 //Your code above ^^^
 
