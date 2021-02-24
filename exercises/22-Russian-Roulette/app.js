0016c78dc161e0cdb99764defb39dc0be5f33c5b
jgustavoj <gjime011@fiu.edu>
2021-02-24 01:11:21 +0000

    practice

--- exercises/22-Russian-Roulette/app.js
+++ exercises/22-Russian-Roulette/app.js
@@ -1,14 +1,21 @@
 var bulletPosition = 4;
+console.log(bulletPosition)
 
 const spinChamber = () => {
-	var chamberPosition = Math.floor((Math.random() * 6) + 1);
-	return chamberPosition;
-};
+    var chamberPosition = Math.floor((Math.random() * 6) + 1);
+        return chamberPosition;
+}
+let spinChamberResult = spinChamber()
+console.log(spinChamberResult)
 
-// Remove the // below and complete the commented lines
 const fireGun = (spinnerPosition) => {
-    //if (...) return ("You're dead!");
-    //else return ("Keep playing!");
+    if(bulletPosition == spinChamberResult){
+        return ("You're dead!");
+
+    } else {
+        return ("Keep playing!")
+    }
+
 };
 
-//console.log(fireGun(...));
+console.log(fireGun())
